6719bee20361e766a5902dbc857d14050a92eadb
shreya <shreya@cedcoss.com>
2022-09-14 12:00:22 +0530

    javascript assignment

---
+++ script.js
@@ -0,0 +1,31 @@
+// var Entry = document.getElementById ("Entry");
+//  Entry.addEventListener ("click",displayDetails);
+// var row = 1;
+// function displayDetails (){
+//     var  ProductId = document.getElementById(" Product Id").value;
+//     var ProductName  = document.getElementById(" Product Name").value; 
+//     var  Productprice = document.getElementById("Product price ").value;
+//     // document.getElementById("Textbox id").Text= "Product Id  " ;
+//     console.log(ProductId)
+
+//     if((ProductId=="")&& (ProductName =="" )&& (Productprice=="")){
+//         alert ("please fill all the boxes");
+//         return;
+//     }
+//     function AddProduct() {
+           
+               
+//         var MyTable =
+//             document.getElementById("display");
+       
+         // insert new row.
+//          var NewRow = MyTable.insertRow(0);
+//          var Newcell1 = NewRow.insertCell(0);
+//        var Newcell2 = NewRow.insertCell(1);
+        
+    
+//     }
+    
+    
+
+// }
